bcbc89f64aa77e27c94e1376f1452fd876336391
GabrielManzano-git <gbot5k@gmail.com>
2026-02-19 18:40:38 -0600

    Added player question boards

--- JeopardyBoard.java
+++ JeopardyBoard.java
@@ -1,8 +1,15 @@
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayer;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -11,36 +18,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JeopardyBoard extends JFrame implements ActionListener{
-    List<JLabel> categoryNames = new ArrayList<>();
-    List<JeopardyBoardButton> buttons = new ArrayList<>();
-    GridLayout gl = new GridLayout(6, 5);
     Color backgroundColor = new Color(0x060ce9);
     Color fontColor = new Color(0xFFD32C);
     int fontSize = 28;
     Font defaultFont = new Font("UTM Helvetins", Font.BOLD, fontSize);
+
+    List<JLabel> categoryNames = new ArrayList<>();
+    List<JeopardyBoardButton> buttons = new ArrayList<>();
+    JeopardyBoardButton currentButton = null;
+    
+    GridLayout gl = new GridLayout(6, 5);
+    
     ImageIcon jpdIcon = new ImageIcon("JPD.png");
+    JPanel questionBoard = new JPanel();
+    CardLayout cl = new CardLayout();
+    JLayeredPane layeredPane = new JLayeredPane();
 
     public JeopardyBoard(){
+        //Create main window with a card layout
+        this.setLayout(cl);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(gl);
         this.setSize(1024, 512);
         this.setResizable(false);
         this.setBackground(Color.BLACK);
         this.setIconImage(jpdIcon.getImage());
 
 
+        //Prepare the question board
+        questionBoard.setLayout(gl);
+        layeredPane.setLayout(new BorderLayout());
+        layeredPane.add(questionBoard, null, JLayeredPane.DEFAULT_LAYER);
+        this.add(layeredPane);
+
+        //Create sample column names and buttons
         LabelFactory(5);
         for(var l : categoryNames){
-            this.add(l);
+            questionBoard.add(l);
         }
 
 
         ButtonFactory(5, 5);
         for(var b : buttons){
-            this.add(b);
+            questionBoard.add(b);
         }
 
+        //Create area for player scoreboards
+        JPanel scoreBoardPanel = new JPanel();
+        scoreBoardPanel.setLayout(new FlowLayout());
+        scoreBoardPanel.setBackground(backgroundColor);
+        layeredPane.add(scoreBoardPanel, BorderLayout.SOUTH, JLayeredPane.PALETTE_LAYER);
 
+
+        //Create sample player scoreboard
+        PlayerScoreboard sampleBoard = new PlayerScoreboard("Test name", this);
+        scoreBoardPanel.add(sampleBoard);
+
+        PlayerScoreboard sampleBoard2 = new PlayerScoreboard("Test name 2", this);
+        scoreBoardPanel.add(sampleBoard2);
+
+
+        //Create Q/A scene
+
+
+        //Show board
         this.setVisible(true);
     }
 
@@ -61,7 +101,9 @@ public class JeopardyBoard extends JFrame implements ActionListener{
             for(int j = 0; j < columns; j++){
                 JeopardyBoardButton newButton = new JeopardyBoardButton(i, j);
                 newButton.setText("$" + newButton.getPointValue());
-                newButton.addActionListener(this);
+                newButton.addActionListener(e -> {
+                    currentButton = newButton;
+                    System.out.println("You pressed a button of value: " + newButton.getPointValue());});
                 newButton.setBackground(backgroundColor);
                 newButton.setForeground(fontColor);
                 newButton.setFont(defaultFont);
@@ -69,6 +111,10 @@ public class JeopardyBoard extends JFrame implements ActionListener{
             }
         }
     } 
+
+    public JeopardyBoardButton getCurrentButton(){
+        return currentButton;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
---
+++ PlayerScoreboard.java
@@ -0,0 +1,73 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PlayerScoreboard extends JPanel{
+    int score = 0;
+    String name;
+    JTextField textField;
+    JeopardyBoard board;
+
+    public PlayerScoreboard(String name, JeopardyBoard board){
+        super();
+
+        this.board = board;
+        this.setBackground(Color.lightGray);
+
+        this.setPreferredSize(new Dimension(160, 70));
+        this.setLayout(new FlowLayout());
+
+        //Create team name
+        this.name = name;
+        JLabel nameLabel = new JLabel(name);
+        this.add(nameLabel);
+        nameLabel.setVerticalAlignment(JLabel.TOP);
+        nameLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        //Create point counter
+        textField = new JTextField();
+        textField.setPreferredSize(new Dimension(50, 20));;
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        textField.setText("" + score);
+        textField.addActionListener(e -> changePoints());
+        this.add(textField);
+
+
+        //Create add/subtract point buttons
+        JButton addButton = new JButton("add");
+        JButton subtractButton = new JButton("sub");
+        this.add(addButton);
+        this.add(subtractButton);
+        addButton.setFocusable(false);
+        subtractButton.setFocusable(false);
+        addButton.addActionListener(e -> {changePoints(); addPoints();});
+        subtractButton.addActionListener(e -> {changePoints(); subtractPoints();});
+    }
+
+    private void changePoints(){
+        score = Integer.parseInt(textField.getText());
+    }
+
+    private void addPoints(){
+        JeopardyBoardButton currQuestion = board.getCurrentButton();
+        if(currQuestion == null) return;
+        int currQuestionPoints = currQuestion.getPointValue();
+
+        score += currQuestionPoints;
+        textField.setText("" + score);
+    }
+
+    private void subtractPoints(){
+        JeopardyBoardButton currQuestion = board.getCurrentButton();
+        if(currQuestion == null) return;
+        int currQuestionPoints = currQuestion.getPointValue();
+
+        score -= currQuestionPoints;
+        textField.setText("" + score);
+    }
+}
